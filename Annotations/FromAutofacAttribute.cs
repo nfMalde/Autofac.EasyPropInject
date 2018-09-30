55eaa3e07cb3096ea492478c985d8b8450741f31
Malte Peters <malte@networkfreaks.de>
2018-09-30 14:45:06 +0200

    Initial Commit

---
+++ Annotations/FromAutofacAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autofac.EasyPropInject.Annotations
+{
+    [System.AttributeUsage(System.AttributeTargets.Property,
+                   AllowMultiple = false,
+                   Inherited = true)]
+    public class FromAutofacAttribute:Attribute
+    {
+    }
+}
---
+++ EasyPropInject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Autofac;
+using Autofac.Builder;
+using System.Linq;
+using System.Reflection;
+
+namespace Autofac.EasyPropInject
+{
+    public static class EasyPropInject
+    {
+        public static ContainerBuilder AddEasyPropInject(this ContainerBuilder builder)
+        {
+            builder.RegisterCallback(c =>
+            {
+                c.Registered += (sender, args) =>
+                {
+                    args.ComponentRegistration.Activated += (o, eventArgs) =>
+                     {
+                         var instance = eventArgs.Instance;
+                         if(instance != null)
+                         {
+                             PropertyInfo[] propertyInfos = instance.GetType().GetProperties();
+                             if(propertyInfos.Any(p => p.GetCustomAttribute<Annotations.FromAutofacAttribute>() != null))
+                             {
+                                 propertyInfos.Where(p => p.GetCustomAttribute<Annotations.FromAutofacAttribute>() != null)
+                                 .ToList()
+                                 .ForEach(p =>
+                                 {
+
+                                     Type propType = p.PropertyType;
+
+                                     object propInstance = null;
+                                     if (eventArgs.Context.TryResolve(propType, out propInstance))
+                                     {
+                                         p.SetValue(instance, propInstance);
+                                     }
+                                 });
+                             }
+                         }
+                     };
+                };
+            });
+
+            return builder;
+        }
+    }
+}
